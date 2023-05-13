3a3d603a8f050634980a4f654edec186f0e7e4f7
chue wathan kyaw <chuewathan.k24@gmail.com>
2023-05-13 13:13:45 +0630

    chue

--- tailwind/src/secenes/Home/Home.jsx
+++ tailwind/src/secenes/Home/Home.jsx
@@ -1,9 +1,67 @@
-const Home = () => {
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+import { Helmet } from 'react-helmet';
 
-    return (
-        <>
-        
-        </>
-    )
- }
+const Home = () => {
+  return (
+    <>
+      <nav className="homenav">
+        {/* <ul className="ul">
+          <li className="li">
+            <div className="link">
+              <span>Noddles</span>
+              <ArrowForwardIcon />
+              <div className="hover-reveal image01">
+                <img
+                  className="hidden-img"
+                  src="assets/project-2.jpeg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </li>
+          <li className="li">
+            <div className="link">
+              <span>Bubble Tea</span>
+              <ArrowForwardIcon />
+              <div className="hover-reveal image01">
+                <img
+                  className="hidden-img"
+                  src="assets/project-3.jpeg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </li>
+          <li className="li">
+            <div className="link">
+              <span>Hambuger</span>
+              <ArrowForwardIcon />
+              <div className="hover-reveal image01">
+                <img
+                  className="hidden-img"
+                  src="assets/project-4.jpeg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </li>
+          <li className="li">
+            <div className="link">
+              <span>Malar</span>
+              <ArrowForwardIcon />
+              <div className="hover-reveal image01">
+                <img
+                  className="hidden-img"
+                  src="assets/project-5.jpeg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </li>
+        </ul> */}
+      </nav>
+      {/* <Helmet> <script src="../../../public/home.js" type="text/javascript"/></Helmet> */}
+    </>
+  );
+};
 export default Home;
